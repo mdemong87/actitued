a7469b680c2134d186ea534ec22c617544275ebd
Md Emon Hossen <mdemong87@gmail.com>
2023-04-16 16:29:04 +0600

    initial commit

---
+++ src/pages/individual.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function individual() {
+  return (
+    <div>individual</div>
+  )
+}
---
+++ src/pages/ngo.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ngo() {
+  return (
+    <div>ngo</div>
+  )
+}
